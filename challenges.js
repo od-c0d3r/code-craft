0b516416d5d64702c6cbf2bcef4d950ee19b1dc2
od-c0d3r <omar_rashad@aol.com>
2022-01-08 08:58:29 +0200

    Save: before new challenge

--- challenges.js
+++ challenges.js
@@ -1,6 +1,9 @@
-// ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+// ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 
+// letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
 
-// Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+// Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or
+// special characters included in the string, they should be returned as they are. 
+// Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
 
 
 function rot13(message) {
@@ -8,5 +11,4 @@ function rot13(message) {
 }
 
 console.log("Grfg" == rot13("Test"));
-
 console.log("grfg" == rot13("test"));
