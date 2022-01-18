d4d0ebf9886df77290b40be286e66c0003ad14ad
od-c0d3r <omar_rashad@aol.com>
2022-01-18 07:35:57 +0200

    Solved: codewars replace regex kata

---
+++ challenges.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/5375f921003bf62192000746/javascript
+
+/*
+The word i18n is a common abbreviation of internationalization in the 
+developer community, used instead of typing the whole word and trying 
+to spell it correctly. Similarly, a11y is an abbreviation of accessibility.
+
+Write a function that takes a string and turns any and all "words" (see below) 
+within that string of length 4 or greater into an abbreviation, following these rules:
+
+    A "word" is a sequence of alphabetical characters. By this definition, any other 
+    character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
+
+    The abbreviated version of the word should have the first letter, 
+    then the number of removed characters, then the last letter 
+    (eg. "elephant ride" => "e6t r2e").
+
+*/
+
+function abbreviate(string) {
+    return string.replace(/[a-zA-Z]{4,}/g, (word) => `${word[0]}${word.length - 2}${word[word.length - 1]}`);
+}
+
+
+console.log(abbreviate("internationalization")); //, "i18n"
+console.log(abbreviate("accessibility")); // , "a11y");
+console.log(abbreviate("Accessibility")); // , "A11y");
+console.log(abbreviate("elephant-ride")); // , "e6t-r2e");
+console.log(abbreviate("elephant-rides are really fun!"));// "e6t""r3s"  "are"  "r4y"   "fun"
