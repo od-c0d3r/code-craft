b051ff8e53776ed799340cca33ebf5fa2bc6099e
od-c0d3r <omar_rashad@aol.com>
2022-01-25 07:40:18 +0200

    Solved: reverse string

---
+++ challenges.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+// Complete the solution so that it reverses the string passed into it. 
+
+function solution(str){
+    return str.split('').reverse().join('');
+}
+
+
+
+solution('world') // 'dlrow'
