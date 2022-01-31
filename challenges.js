ea9c6baadcc066d73472bc698da4029bb1f24ce1
od-c0d3r <omar_rashad@aol.com>
2022-01-31 05:10:30 +0200

    Solve: reverse word in place

--- challenges.js
+++ challenges.js
@@ -1,11 +1,19 @@
 // https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
 
-// Complete the solution so that it reverses the string passed into it. 
+ /**
+  *Complete the function that accepts a string parameter, 
+  and reverses each word in the string. 
+  All spaces in the string should be retained.
+  */
 
-function solution(str){
-    return str.split('').reverse().join('');
-}
+const reverseWords = (str) => str.split(' ').map((word) => word.split('').reverse().join('')).join(' ');
 
 
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.')  === 'ehT kciuq nworb xof spmuj revo eht yzal .god');
 
-solution('world') // 'dlrow'
+
+/**
+ * i started by the knowleddge of reverse and split array methods
+ * and we wanna reverse each word on its orginal postion, 
+ * so we gonna use map method too on the array
+ */
