52cbd43816843efcde076646b1dceb78de06d58b
od-c0d3r <omar_rashad@aol.com>
2022-02-04 07:47:09 +0200

    Save: start new challenge ~ Fri  4 Feb 07:47:09 EET 2022

--- challenges.js
+++
@@ -1,8 +0,0 @@
-// https://www.codewars.com/kata/5556282156230d0e5e000089/train/javascript
-
-
-function DNAtoRNA(dna) {
-    return dna.replaceAll('T','U');
-}
-
-console.log(DNAtoRNA("TTTT") === "UUUU");
