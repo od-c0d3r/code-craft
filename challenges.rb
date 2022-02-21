455c76db01e62d5f9924b60f7e536742d505c059
od-c0d3r <omar_rashad@aol.com>
2022-02-21 08:45:09 +0200

    Ruby: solved replace chars

--- challenges.rb
+++ challenges.rb
@@ -1,27 +1,7 @@
-# // // https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/ruby
+# // https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/ruby
 
-=begin
-Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
-
-Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
-
-Example:
-
-Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
-Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
-
-=end
-
-class String
-  def toJadenCase
-    new_str = self.split(' ').each do |word|
-      word.capitalize!
-    end
-    new_str.join(' ')
-  end
+def DNA_strand(dna)
+  puts dna.tr('ATGC', 'TACG')  
 end
 
-str = "How can mirrors be real if our eyes aren't real";
-
-puts str.toJadenCase()
-
+DNA_strand("AAAA")
