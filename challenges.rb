0045c4b4c8a5e257a358fe43456787ab7efb753b
od-c0d3r <omar_rashad@aol.com>
2022-02-28 04:46:57 +0200

    Ruby: sum generated array of nums bassed on input

--- challenges.rb
+++ challenges.rb
@@ -1,7 +1,25 @@
-# // https://www.codewars.com/kata/555eded1ad94b00403000071/train/ruby
+# https://www.codewars.com/kata/555eded1ad94b00403000071/train/ruby
 
-def DNA_strand(dna)
-  puts dna.tr('ATGC', 'TACG')  
+def series_sum(n)
+  sum = []
+  denominator = 4;
+  
+  # Need to be optimized (include it in the for loop)
+  return '0.00' if n == 0
+  return '1.00' if n == 1
+  
+  # Proccessing the Sum array that contains the series
+  for i in 1..n
+    rat = Rational(1, denominator)
+    if i == 1
+      sum.push(1)
+    else
+      sum.push(rat)
+      denominator += 3;
+    end
+  end
+
+  puts sprintf('%.2f', sum.sum)
 end
 
-DNA_strand("AAAA")
+series_sum(3)
