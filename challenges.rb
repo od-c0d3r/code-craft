38d7651b44b9d8f46a74d1263b1f43d2da5c5aeb
od-c0d3r <omar_rashad@aol.com>
2022-03-01 06:44:38 +0200

    Ruby: population calculating

--- challenges.rb
+++ challenges.rb
@@ -1,25 +1,44 @@
-# https://www.codewars.com/kata/555eded1ad94b00403000071/train/ruby
+# https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/ruby
+# In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
 
-def series_sum(n)
-  sum = []
-  denominator = 4;
-  
-  # Need to be optimized (include it in the for loop)
-  return '0.00' if n == 0
-  return '1.00' if n == 1
-  
-  # Proccessing the Sum array that contains the series
-  for i in 1..n
-    rat = Rational(1, denominator)
-    if i == 1
-      sum.push(1)
-    else
-      sum.push(rat)
-      denominator += 3;
-    end
+# At the end of the first year there will be: 
+# 1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+# At the end of the 2nd year there will be: 
+# 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+
+# At the end of the 3rd year there will be:
+# 1141 + 1141 * 0.02 + 50 => 1213
+
+# It will need 3 entire years.
+
+# More generally given parameters:
+
+# p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+
+# the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+# aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+# Examples:
+# nb_year(1500, 5, 100, 5000) -> 15
+# nb_year(1500000, 2.5, 10000, 2000000) -> 10
+
+# Note:
+
+# Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+
+def nb_year(p0, percent, aug, p)
+  new_p0 = p0
+  years = 0
+  percent = percent * 0.01 
+
+  until new_p0 >= p do
+    new_p0 += (new_p0*percent).to_i + aug
+    years += 1
   end
 
-  puts sprintf('%.2f', sum.sum)
+  years
 end
 
-series_sum(3)
+nb_year(1000, 2.0, 50, 1214) # 4 years
