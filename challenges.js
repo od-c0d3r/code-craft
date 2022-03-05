4758dc8a5558466faaf68dfbea82c44120699140
od-c0d3r <omar_rashad@aol.com>
2022-03-05 07:40:19 +0200

    Js: even or odd

--- challenges.js
+++ challenges.js
@@ -1,1 +1,11 @@
-// https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/ruby
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+// Create a function that takes an integer 
+// as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+    return number % 2 == 0 ? "Even" : "Odd";
+}
+
+console.log(even_or_odd(2));
+console.log(even_or_odd(3));
