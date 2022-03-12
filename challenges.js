cef5843c210ec0defc8d290a6dbb1e1e5672cd79
od-c0d3r <omar_rashad@aol.com>
2022-03-12 10:01:46 +0200

    Save: start new challenge ~ Sat 12 Mar 10:01:46 EET 2022

--- challenges.js
+++
@@ -1,7 +0,0 @@
-// https://www.codewars.com/kata/5389864ec72ce03383000484/train/javascript
-
-function autocomplete(input, dictionary){
-    return dictionary.filter( word => word.toLowerCase().match(input))
-}
-
-console.log(autocomplete('ai', ['airplane','airport','apple','ball'])) // == ['airplane','airport']
