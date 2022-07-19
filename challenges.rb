8027d0aedd6027e2e09895690503cf27a20c07d1
Omar R <o.rashad2020@gmail.com>
2022-07-19 06:55:27 +0200

    Ruby: shortest word

--- challenges.rb
+++ challenges.rb
@@ -1,36 +1,15 @@
-# https://www.codewars.com/kata/52449b062fb80683ec000024/train/ruby
+# https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/ruby
 
-# The marketing team is spending way too much time typing in hashtags.
-# Let's help them with our own Hashtag Generator!
+# Simple, given a string of words, return the length of the shortest word(s).
 
-# Here's the deal:
+# String will never be empty and you do not need to account for different data types.
 
-#     It must start with a hashtag (#).
-#     All words must have their first letter capitalized.
-#     If the final result is longer than 140 chars it must return false.
-#     If the input or the result is an empty string it must return false.
-
-# Examples
-
-# " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
-# "    Hello     World   "                  =>  "#HelloWorld"
-# ""                                        =>  false
-
-def generateHashtag(str)
-    if str.split(' ').empty? || str.split(' ').join('').length >= 140
-        return false 
-    else
-        hash = '#'
-        str.split(' ').each do |char|
-            hash << char.capitalize
-        end
-    end
-    hash
+def find_short(s)
+    l = 100
+    s.split(" "){ |word| l = word.length if l > word.length }
+    l
 end
 
-# p generateHashtag("a" * 140) # false, "Too long")
-# p generateHashtag("code" + " " * 140 + "wars") #, "#CodeWars")
+# find_short("bitcoin take over the world maybe who knows perhaps") > 3
 
-generateHashtag("Do We have A Hashtag")
-
-#, "#DoWeHaveAHashtag", "Expected a Hashtag (#) at the beginning.")
+puts find_short("Do We have aaaaaa Hashtag") #1
