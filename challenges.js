ae9ac77dee674273d14d929b1cc8220356fae541
Omar R <o.rashad2020@gmail.com>
2022-07-20 20:49:49 +0200

    Ruby: words weight

--- challenges.js
+++ challenges.js
@@ -1,1 +1,4 @@
 // https://www.codewars.com/kata/517abf86da9663f1d2000003/train/ruby
+
+
+console.log("heyo")
--- challenges.rb
+++ challenges.rb
@@ -1,21 +1,38 @@
-# https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/ruby
+# https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/ruby
 
-# Simple, given a string of words, return the length of the shortest word(s).
+# Given a string of words, you need to find the highest scoring word.
 
-# String will never be empty and you do not need to account for different data types.
+# Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
 
-def find_short(s)
-    l = 100
-    s.split(" "){ |word| l = word.length if l > word.length }
-    l
+# You need to return the highest scoring word as a string.
+
+# If two words score the same, return the word that appears earliest in the original string.
+
+# All letters will be lowercase and all inputs will be valid.
+
+def high(x)
+  alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  biggest_word_weight = 0;
+  biggest_word_index = 0;
+  current_word_weight = 0;
+  new_x = x.split(' ')
+
+  new_x.each_with_index do |word, index|
+
+    word.split('') do |char|
+      current_word_weight += alphabet.index(char)
+    end
+
+    if current_word_weight > biggest_word_weight
+      biggest_word_weight = current_word_weight;
+      biggest_word_index = index;
+    end
+
+    p "#{word} #{current_word_weight}"
+    current_word_weight = 0
+  end
+
+  new_x[biggest_word_index]
 end
 
-# find_short("bitcoin take over the world maybe who knows perhaps") > 3
-
-puts find_short("Do We have aaaaaa Hashtag") #1
-
-
-=begin
-better solution 
-s.split.map(&:size).min   
-=end
+p high('man i need a taxi up to ubud') # taxi
