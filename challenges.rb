1fc538d0967a52fb7f1a0df8e52c45a66a695e2f
Omar R <omar_rashad@aol.com>
2025-09-23 10:50:59 +0300

    ruby: solve two sum using sliding window

--- challenges.rb
+++ challenges.rb
@@ -1,35 +1,28 @@
-# # https://www.codewars.com/kata/51edd51599a189fe7f000015/train/ruby
-# Complete the function that
 
-# accepts two integer arrays of equal length
-# compares the value each member in one array to the corresponding member in the other
-# squares the absolute value difference between those two values
-# and returns the average of those squared absolute value difference between each member pair.
-# Examples
-
-# [1, 2, 3], [4, 5, 6]              -->   9   because (9 + 9 + 9) / 3
-# [10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
-# [-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2
-
-def solution(arr1, arr2)
-  # Allocate a new arary based on one of the arraies length
-  # Make a loop from Zero to array length
-  #   Access each value using the index in both arrays
-  #   Make the math of (x - y)^2
-  #   Store the value in the new array using the index
-  # Make a loop on the result array to get the avg.
-  #
-  #
-  #
-  # result = Array.new(arr1.length) { |index| index }
-
-  # result.reduce(0) { |memo, index|
-  #   p memo
-  #   p index
-  #   memo + ((arr1[index] - arr2[index])**2)
-  # }
+def two_sum(nums, target)
+  # loop over the array with first pointer
+  # create a new loop from pointer to the array size
+  # if first pointer + 2nd pointer = target
+  # then return [1st pointer, 2nd pointer]
+  (0...nums.size).each do |first_pointer|
+    (first_pointer + 1...nums.size).each do |second_pointer|
+      return [first_pointer, second_pointer] if nums[first_pointer] + nums[second_pointer] == target
+    end
+  end
 end
 
-solution([1, 2, 3], [4, 5, 6]) # 9
-# p solution([10, 20, 10, 2], [10, 25, 5, -2]) # 16.5
-# p solution([-1, 0], [0, -1]) # 9
+# nums =
+# [2,7,11,15]
+# target =
+# 9
+# Expected
+# [0,1]
+
+# nums =
+# [3,2,4]
+# target =
+# # 6
+# Expected
+# [1,2]
+
+p two_sum([3, 2, 4], 6)
